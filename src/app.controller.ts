86874e2c2eeae75593fa6aedbeb0d045ffed51da
Petrik Diak <diak@petrik.hu>
2024-10-07 13:49:56 +0200

    Current state 10. 07. 13:49

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Render('index')
@@ -11,5 +11,49 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  @Get('success')
+  @Render('success')
+  getSuccess() {
+    return {
+      message: "Sikeres megrendeles!"
+    };
+  }
+
+  @Post('submit')
+  handleSubmit(@Body() body: any) {
+    const name = body.name;
+    const billingCountry = body.billingCountry;
+    const billingZip = body.billingZip;
+    const billingCity = body.billingCity;
+    const billingStreet = body.billingStreet;
+    const shippingCountry = body.shippingCountry;
+    const shippingZip = body.shippingZip;
+    const shippingCity = body.shippingCity;
+    const shippingStreet = body.shippingStreet;
+    const couponCode = body.couponCode;
+    const cardNumber = body.cardNumber;
+    const expiryDate = body.expiryDate;
+    const securityCode = body.securityCode;
+
+    const date = new Date();
+    if (date.getFullYear > expiryDate || (date.getFullYear == expiryDate && date.getMonth() > expiryDate)) {
+      return "Lejart kartya";
+    }
+
+    const couponCodeRegExp = /^[A-Z]{2}-\d{4}$/;
+    if (!couponCodeRegExp.test(couponCode)) {
+      return "Nem letezo kupon kod";
+    }
+
+    const cardNumberRegExp = /^\d{4}-\d{4}-\d{4}-\d{4}$/;
+    if (!cardNumberRegExp.test(cardNumber)) {
+      return "Hibas kartyaszam formatum";
+    }
+
+    if (securityCode.length > 3) {
+      return "Hibas biztonsagi kod";
+    }
   }
 }
